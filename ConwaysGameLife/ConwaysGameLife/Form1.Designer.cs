9d6adcf74cba405d0e1f2dfff85e1997f474cc6b
dmitry741 <dmitrypavlov74@gmail.com>
2016-10-24 16:26:20 +0300

    add Save map button

--- ConwaysGameLife/ConwaysGameLife/Form1.Designer.cs
+++ ConwaysGameLife/ConwaysGameLife/Form1.Designer.cs
@@ -34,6 +34,7 @@
             this.cmbSceneMode = new System.Windows.Forms.ComboBox();
             this.button1 = new System.Windows.Forms.Button();
             this.btnStop = new System.Windows.Forms.Button();
+            this.frmSave = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -90,11 +91,21 @@
             this.btnStop.Text = "Stop";
             this.btnStop.UseVisualStyleBackColor = true;
             // 
+            // frmSave
+            // 
+            this.frmSave.Location = new System.Drawing.Point(724, 190);
+            this.frmSave.Name = "frmSave";
+            this.frmSave.Size = new System.Drawing.Size(124, 38);
+            this.frmSave.TabIndex = 6;
+            this.frmSave.Text = "Save map...";
+            this.frmSave.UseVisualStyleBackColor = true;
+            // 
             // frmMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(909, 730);
+            this.Controls.Add(this.frmSave);
             this.Controls.Add(this.btnStop);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.cmbSceneMode);
@@ -120,6 +131,7 @@
         private System.Windows.Forms.ComboBox cmbSceneMode;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Button frmSave;
     }
 }
 
