197c27d766b01fd0776a2b7ae01a01ba5a6f81c1
dmitry741 <dmitrypavlov74@gmail.com>
2016-10-24 17:03:05 +0300

    Add load map button

--- ConwaysGameLife/ConwaysGameLife/Form1.Designer.cs
+++ ConwaysGameLife/ConwaysGameLife/Form1.Designer.cs
@@ -35,6 +35,7 @@
             this.button1 = new System.Windows.Forms.Button();
             this.btnStop = new System.Windows.Forms.Button();
             this.frmSave = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -101,11 +102,21 @@
             this.frmSave.Text = "Save map...";
             this.frmSave.UseVisualStyleBackColor = true;
             // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(724, 234);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(124, 38);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Load map...";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
             // frmMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(909, 730);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.frmSave);
             this.Controls.Add(this.btnStop);
             this.Controls.Add(this.button1);
@@ -133,6 +144,7 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button btnStop;
         private System.Windows.Forms.Button frmSave;
+        private System.Windows.Forms.Button button2;
     }
 }
 
