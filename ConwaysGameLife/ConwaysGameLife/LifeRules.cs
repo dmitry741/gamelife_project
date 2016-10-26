c8512767f06048a909b8ce80554559f6498dc000
dmitry741 <dmitrypavlov74@gmail.com>
2016-10-26 11:56:06 +0300

    add liferules file

---
+++ ConwaysGameLife/ConwaysGameLife/LifeRules.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConwaysGameLife
+{
+    interface ILifeRule
+    {
+        int NewCell(int neighbors);
+        int ContinueLife(int neighbors);
+    }
+
+    class ClassicConwaysRules : ILifeRule
+    {
+        public int NewCell(int neighbors)
+        {
+            return (neighbors == 3) ? 1 : 0;
+        }
+
+        public int ContinueLife(int neighbors)
+        {
+            return (neighbors == 2 || neighbors == 3) ? 2 : 0;
+        }
+    }
+}
