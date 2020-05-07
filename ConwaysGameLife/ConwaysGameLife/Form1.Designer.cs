d16aa0274a4c763479054a6de232eff030837650
Dmitriy Pavlov <dmitriy.pavlov@danfoss.com>
2020-05-07 16:41:12 +0300

    Change the form size

--- ConwaysGameLife/ConwaysGameLife/Form1.Designer.cs
+++ ConwaysGameLife/ConwaysGameLife/Form1.Designer.cs
@@ -62,10 +62,9 @@
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(18, 18);
-            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(1059, 1086);
+            this.pictureBox1.Size = new System.Drawing.Size(706, 706);
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             this.pictureBox1.SizeChanged += new System.EventHandler(this.pictureBox1_SizeChanged);
@@ -73,10 +72,9 @@
             // 
             // btnStart
             // 
-            this.btnStart.Location = new System.Drawing.Point(14, 29);
-            this.btnStart.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnStart.Location = new System.Drawing.Point(9, 19);
             this.btnStart.Name = "btnStart";
-            this.btnStart.Size = new System.Drawing.Size(216, 43);
+            this.btnStart.Size = new System.Drawing.Size(144, 28);
             this.btnStart.TabIndex = 1;
             this.btnStart.Text = "Старт";
             this.btnStart.UseVisualStyleBackColor = true;
@@ -85,10 +83,9 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(1086, 18);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Location = new System.Drawing.Point(724, 12);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(59, 20);
+            this.label1.Size = new System.Drawing.Size(42, 13);
             this.label1.TabIndex = 2;
             this.label1.Text = "Режим";
             // 
@@ -96,18 +93,16 @@
             // 
             this.cmbSceneMode.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.cmbSceneMode.FormattingEnabled = true;
-            this.cmbSceneMode.Location = new System.Drawing.Point(1095, 43);
-            this.cmbSceneMode.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.cmbSceneMode.Location = new System.Drawing.Point(730, 28);
             this.cmbSceneMode.Name = "cmbSceneMode";
-            this.cmbSceneMode.Size = new System.Drawing.Size(229, 28);
+            this.cmbSceneMode.Size = new System.Drawing.Size(154, 21);
             this.cmbSceneMode.TabIndex = 3;
             // 
             // btnExit
             // 
-            this.btnExit.Location = new System.Drawing.Point(1172, 1062);
-            this.btnExit.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnExit.Location = new System.Drawing.Point(781, 690);
             this.btnExit.Name = "btnExit";
-            this.btnExit.Size = new System.Drawing.Size(152, 43);
+            this.btnExit.Size = new System.Drawing.Size(101, 28);
             this.btnExit.TabIndex = 4;
             this.btnExit.Text = "Выход";
             this.btnExit.UseVisualStyleBackColor = true;
@@ -115,10 +110,9 @@
             // 
             // btnSave
             // 
-            this.btnSave.Location = new System.Drawing.Point(9, 86);
-            this.btnSave.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnSave.Location = new System.Drawing.Point(6, 56);
             this.btnSave.Name = "btnSave";
-            this.btnSave.Size = new System.Drawing.Size(220, 43);
+            this.btnSave.Size = new System.Drawing.Size(147, 28);
             this.btnSave.TabIndex = 6;
             this.btnSave.Text = "Сохранить карту...";
             this.btnSave.UseVisualStyleBackColor = true;
@@ -126,10 +120,9 @@
             // 
             // btnLoad
             // 
-            this.btnLoad.Location = new System.Drawing.Point(9, 138);
-            this.btnLoad.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnLoad.Location = new System.Drawing.Point(6, 90);
             this.btnLoad.Name = "btnLoad";
-            this.btnLoad.Size = new System.Drawing.Size(220, 43);
+            this.btnLoad.Size = new System.Drawing.Size(147, 28);
             this.btnLoad.TabIndex = 7;
             this.btnLoad.Text = "Загрузить карту...";
             this.btnLoad.UseVisualStyleBackColor = true;
@@ -142,21 +135,18 @@
             this.groupBox1.Controls.Add(this.btnSave);
             this.groupBox1.Controls.Add(this.btnLoad);
             this.groupBox1.Controls.Add(this.btnRandom);
-            this.groupBox1.Location = new System.Drawing.Point(1086, 98);
-            this.groupBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.groupBox1.Location = new System.Drawing.Point(724, 64);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Padding = new System.Windows.Forms.Padding(4, 5, 4, 5);
-            this.groupBox1.Size = new System.Drawing.Size(238, 302);
+            this.groupBox1.Size = new System.Drawing.Size(159, 196);
             this.groupBox1.TabIndex = 8;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Карты";
             // 
             // btnSaveImage
             // 
-            this.btnSaveImage.Location = new System.Drawing.Point(9, 34);
-            this.btnSaveImage.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnSaveImage.Location = new System.Drawing.Point(6, 22);
             this.btnSaveImage.Name = "btnSaveImage";
-            this.btnSaveImage.Size = new System.Drawing.Size(220, 43);
+            this.btnSaveImage.Size = new System.Drawing.Size(147, 28);
             this.btnSaveImage.TabIndex = 9;
             this.btnSaveImage.Text = "Сохранить...";
             this.btnSaveImage.UseVisualStyleBackColor = true;
@@ -164,10 +154,9 @@
             // 
             // btnClear
             // 
-            this.btnClear.Location = new System.Drawing.Point(9, 243);
-            this.btnClear.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnClear.Location = new System.Drawing.Point(6, 158);
             this.btnClear.Name = "btnClear";
-            this.btnClear.Size = new System.Drawing.Size(220, 43);
+            this.btnClear.Size = new System.Drawing.Size(147, 28);
             this.btnClear.TabIndex = 8;
             this.btnClear.Text = "Очистить карту...";
             this.btnClear.UseVisualStyleBackColor = true;
@@ -175,10 +164,9 @@
             // 
             // btnRandom
             // 
-            this.btnRandom.Location = new System.Drawing.Point(9, 191);
-            this.btnRandom.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnRandom.Location = new System.Drawing.Point(6, 124);
             this.btnRandom.Name = "btnRandom";
-            this.btnRandom.Size = new System.Drawing.Size(220, 43);
+            this.btnRandom.Size = new System.Drawing.Size(147, 28);
             this.btnRandom.TabIndex = 6;
             this.btnRandom.Text = "Готовые карты...";
             this.btnRandom.UseVisualStyleBackColor = true;
@@ -192,11 +180,9 @@
             this.groupBox2.Controls.Add(this.btnNext10);
             this.groupBox2.Controls.Add(this.btnNext);
             this.groupBox2.Controls.Add(this.btnStart);
-            this.groupBox2.Location = new System.Drawing.Point(1086, 409);
-            this.groupBox2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.groupBox2.Location = new System.Drawing.Point(724, 266);
             this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Padding = new System.Windows.Forms.Padding(4, 5, 4, 5);
-            this.groupBox2.Size = new System.Drawing.Size(238, 292);
+            this.groupBox2.Size = new System.Drawing.Size(159, 190);
             this.groupBox2.TabIndex = 9;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "Игра";
@@ -205,39 +191,35 @@
             // 
             this.cmbNext.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.cmbNext.FormattingEnabled = true;
-            this.cmbNext.Location = new System.Drawing.Point(156, 234);
-            this.cmbNext.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.cmbNext.Location = new System.Drawing.Point(104, 152);
             this.cmbNext.Name = "cmbNext";
-            this.cmbNext.Size = new System.Drawing.Size(54, 28);
+            this.cmbNext.Size = new System.Drawing.Size(37, 21);
             this.cmbNext.TabIndex = 6;
             // 
             // cmbAnimateMode
             // 
             this.cmbAnimateMode.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.cmbAnimateMode.FormattingEnabled = true;
-            this.cmbAnimateMode.Location = new System.Drawing.Point(14, 125);
-            this.cmbAnimateMode.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.cmbAnimateMode.Location = new System.Drawing.Point(9, 81);
             this.cmbAnimateMode.Name = "cmbAnimateMode";
-            this.cmbAnimateMode.Size = new System.Drawing.Size(216, 28);
+            this.cmbAnimateMode.Size = new System.Drawing.Size(145, 21);
             this.cmbAnimateMode.TabIndex = 5;
             this.cmbAnimateMode.SelectedIndexChanged += new System.EventHandler(this.cmbAnimateMode_SelectedIndexChanged);
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(9, 100);
-            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Location = new System.Drawing.Point(6, 65);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(137, 20);
+            this.label2.Size = new System.Drawing.Size(95, 13);
             this.label2.TabIndex = 4;
             this.label2.Text = "Режим анимации";
             // 
             // btnNext10
             // 
-            this.btnNext10.Location = new System.Drawing.Point(14, 226);
-            this.btnNext10.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnNext10.Location = new System.Drawing.Point(9, 147);
             this.btnNext10.Name = "btnNext10";
-            this.btnNext10.Size = new System.Drawing.Size(134, 43);
+            this.btnNext10.Size = new System.Drawing.Size(89, 28);
             this.btnNext10.TabIndex = 3;
             this.btnNext10.Text = "Шаг +=";
             this.btnNext10.UseVisualStyleBackColor = true;
@@ -245,10 +227,9 @@
             // 
             // btnNext
             // 
-            this.btnNext.Location = new System.Drawing.Point(14, 166);
-            this.btnNext.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnNext.Location = new System.Drawing.Point(9, 108);
             this.btnNext.Name = "btnNext";
-            this.btnNext.Size = new System.Drawing.Size(216, 43);
+            this.btnNext.Size = new System.Drawing.Size(144, 28);
             this.btnNext.TabIndex = 2;
             this.btnNext.Text = "Шаг++";
             this.btnNext.UseVisualStyleBackColor = true;
@@ -257,11 +238,9 @@
             // groupBox3
             // 
             this.groupBox3.Controls.Add(this.cbGridView);
-            this.groupBox3.Location = new System.Drawing.Point(1086, 842);
-            this.groupBox3.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.groupBox3.Location = new System.Drawing.Point(724, 547);
             this.groupBox3.Name = "groupBox3";
-            this.groupBox3.Padding = new System.Windows.Forms.Padding(4, 5, 4, 5);
-            this.groupBox3.Size = new System.Drawing.Size(238, 75);
+            this.groupBox3.Size = new System.Drawing.Size(159, 49);
             this.groupBox3.TabIndex = 11;
             this.groupBox3.TabStop = false;
             this.groupBox3.Text = "Отображение";
@@ -271,10 +250,9 @@
             this.cbGridView.AutoSize = true;
             this.cbGridView.Checked = true;
             this.cbGridView.CheckState = System.Windows.Forms.CheckState.Checked;
-            this.cbGridView.Location = new System.Drawing.Point(9, 29);
-            this.cbGridView.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.cbGridView.Location = new System.Drawing.Point(6, 19);
             this.cbGridView.Name = "cbGridView";
-            this.cbGridView.Size = new System.Drawing.Size(81, 24);
+            this.cbGridView.Size = new System.Drawing.Size(56, 17);
             this.cbGridView.TabIndex = 0;
             this.cbGridView.Text = "Сетка";
             this.cbGridView.UseVisualStyleBackColor = true;
@@ -282,10 +260,9 @@
             // 
             // btnSettings
             // 
-            this.btnSettings.Location = new System.Drawing.Point(1084, 928);
-            this.btnSettings.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnSettings.Location = new System.Drawing.Point(723, 603);
             this.btnSettings.Name = "btnSettings";
-            this.btnSettings.Size = new System.Drawing.Size(238, 43);
+            this.btnSettings.Size = new System.Drawing.Size(159, 28);
             this.btnSettings.TabIndex = 12;
             this.btnSettings.Text = "Правила...";
             this.btnSettings.UseVisualStyleBackColor = true;
@@ -297,11 +274,9 @@
             this.groupBox4.Controls.Add(this.label4);
             this.groupBox4.Controls.Add(this.lblCount);
             this.groupBox4.Controls.Add(this.label3);
-            this.groupBox4.Location = new System.Drawing.Point(1086, 711);
-            this.groupBox4.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.groupBox4.Location = new System.Drawing.Point(724, 462);
             this.groupBox4.Name = "groupBox4";
-            this.groupBox4.Padding = new System.Windows.Forms.Padding(4, 5, 4, 5);
-            this.groupBox4.Size = new System.Drawing.Size(238, 122);
+            this.groupBox4.Size = new System.Drawing.Size(159, 79);
             this.groupBox4.TabIndex = 13;
             this.groupBox4.TabStop = false;
             this.groupBox4.Text = "Статистика";
@@ -309,48 +284,44 @@
             // lblStep
             // 
             this.lblStep.AutoSize = true;
-            this.lblStep.Location = new System.Drawing.Point(66, 75);
-            this.lblStep.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblStep.Location = new System.Drawing.Point(44, 49);
             this.lblStep.Name = "lblStep";
-            this.lblStep.Size = new System.Drawing.Size(18, 20);
+            this.lblStep.Size = new System.Drawing.Size(13, 13);
             this.lblStep.TabIndex = 3;
             this.lblStep.Text = "0";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(9, 75);
-            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Location = new System.Drawing.Point(6, 49);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(55, 20);
+            this.label4.Size = new System.Drawing.Size(39, 13);
             this.label4.TabIndex = 2;
             this.label4.Text = "Шаг = ";
             // 
             // lblCount
             // 
             this.lblCount.AutoSize = true;
-            this.lblCount.Location = new System.Drawing.Point(139, 40);
-            this.lblCount.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblCount.Location = new System.Drawing.Point(93, 26);
             this.lblCount.Name = "lblCount";
-            this.lblCount.Size = new System.Drawing.Size(18, 20);
+            this.lblCount.Size = new System.Drawing.Size(13, 13);
             this.lblCount.TabIndex = 1;
             this.lblCount.Text = "0";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(9, 40);
-            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Location = new System.Drawing.Point(6, 26);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(127, 20);
+            this.label3.Size = new System.Drawing.Size(86, 13);
             this.label3.TabIndex = 0;
             this.label3.Text = "Число клеток =";
             // 
             // frmMain
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1336, 1050);
+            this.ClientSize = new System.Drawing.Size(891, 724);
             this.Controls.Add(this.groupBox4);
             this.Controls.Add(this.btnSettings);
             this.Controls.Add(this.groupBox3);
@@ -362,7 +333,6 @@
             this.Controls.Add(this.pictureBox1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.KeyPreview = true;
-            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.MaximizeBox = false;
             this.Name = "frmMain";
             this.Text = "Игра Жизнь";
